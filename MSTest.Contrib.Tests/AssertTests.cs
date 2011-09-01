e1c8e3307f3316f52609f023f4afc1a9832b9bbf
Oleg Demchenko <demchenko.oleg@gmail.com>
2011-08-31 22:28:25 -0700

    * Add nUnit testing project

---
+++ MSTest.Contrib.Tests/AssertTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace MSTest.Contrib.Tests
+{
+    [TestFixture]
+    public class AssertTests
+    {
+        [Test]
+        public void TESTNAME()
+        {
+            
+        }
+    }
+}
